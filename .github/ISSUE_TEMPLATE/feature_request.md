c901f5c4ae78d5f1bc8b5b7aee449a3a33f7d656
fredp4t1qbit <fred.park@1qbit.com>
2023-01-25 12:08:28 -0800

    Setting up guidelines

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,23 @@
+---
+name: Feature request
+about: Suggest an idea for this project
+title: ''
+labels: enhancement
+assignees: ''
+
+---
+
+### Feature Description
+**Please describe the feature you would like to see added:**
+
+[Insert your feature description here]
+
+### Rationale
+**Why is this feature important? What problem does it solve?**
+
+[Insert your rationale here]
+
+### Additional Information
+**Are there any related issues? Are there any specific considerations or constraints?**
+
+[Insert any additional information here]
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ccvm",
+    version="0.1.0",
+    description="Solve continuous non-convex optimization problems with CCVM architectures and solvers",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/1QB-Information-Technologies/ccvm",
+    author="1QBit",
+    author_email="support@1qbit.com",
+    license='GNU Affero General Public License v3.0',
+    packages=find_packages(),
+    install_requires=[
+        "future",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "pytest",
+        "scipy",
+        "torch",
+        "json-stream",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
