843ddb207066ad67bdab0e22b24a2ac2c83f15ea
Katie Olfert <katie.olfert@1qbit.com>
2023-03-03 09:36:36 -0800

    Update the way we read requirements in setup.py
    The original way seemed to require the user to be in the same directory as
    the requirements.txt file when they download it.
    The new way doesn't require the user to have the requirements.txt
    file at all.

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+from pkg_resources import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("requirements.txt")
+
+# convert to list
+required = [str(ir) for ir in install_reqs]
 
 setup(
     name="ccvm",
     version="0.1.0",
     description="Solve continuous non-convex optimization problems with CCVM architectures and solvers",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/1QB-Information-Technologies/ccvm",
     author="1QBit",
     author_email="support@1qbit.com",
-    license='GNU Affero General Public License v3.0',
+    license="GNU Affero General Public License v3.0",
     packages=find_packages(),
     install_requires=required,
     classifiers=[
