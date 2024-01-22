349ddf0f34763526760499cfd98d8f5644616bee
ChanWooY-1QBit <chanwoo.yang@1qbit.com>
2024-01-22 14:07:35 -0800

    TTS plot data calc updated; boxqp metadata class updated; testcases updated

--- ccvm_simulators/ccvmplotlib/tests/test_BoxQP_metadata.py
+++ ccvm_simulators/ccvmplotlib/tests/test_BoxQP_metadata.py
@@ -10,15 +10,12 @@ class TestBoxQPMetadata(TestCase):
     def setUp(self) -> None:
         self.valid_problem_type = ProblemType.BoxQP
         self.invalid_problem_type_str = "CCVM"
-        self.valid_TTS_type = TTSType.physical
-        self.invalid_TTS_type_str = "wall_clock"
 
     def test_BoxQP_metadata_valid(self):
         """Test BoxQP Metadata class object when valid inputs are given."""
-        boxqp_metadata = BoxQPMetadata(self.valid_problem_type, self.valid_TTS_type)
+        boxqp_metadata = BoxQPMetadata(self.valid_problem_type)
 
         self.assertEqual(boxqp_metadata.problem, self.valid_problem_type)
-        self.assertEqual(boxqp_metadata.TTS_type, self.valid_TTS_type)
         self.assertEqual(boxqp_metadata._BoxQPMetadata__problem_size_list, [])
         self.assertEqual(boxqp_metadata._BoxQPMetadata__percent_gap_list, [])
         self.assertEqual(
@@ -35,17 +32,7 @@ class TestBoxQPMetadata(TestCase):
         with self.assertRaises(ValueError):
             BoxQPMetadata(
                 ProblemType(self.invalid_problem_type_str),
-                self.valid_TTS_type,
             )
-
-    def test_BoxQP_metadata_invalid_TTS_type(self):
-        """Test BoxQP Metadata class object when an invalid TTS type is
-        given.
-
-
-        """
-        with self.assertRaises(ValueError):
-            BoxQPMetadata(self.valid_problem_type, TTSType(self.invalid_TTS_type_str))
 
     def test_ingest_result_data_method_valid(self):
         """Test BoxQP Metadata class "ingest_result_data" method when valid
